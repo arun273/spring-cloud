e8a20e0fd9bfa28bc0c5159ba58db3506794f4fd
arun273 <tupuriarun@gmail.com>
2023-04-18 10:26:36 +0530

    commit

---
+++ spring-cloud-gateway-eureka/order-service/src/main/java/com/training/examples/orderService/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.training.examples.orderService.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @GetMapping("list")
+    public String getOrderList() {
+        return "get from order-service:getOrderList()";
+    }
+}
---
+++ spring-cloud-gateway-eureka/user-service/src/main/java/com/training/examples/userService/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.training.examples.userService.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("list")
+    public String getUserList() {
+        return "get from user-service:getUserList()";
+    }
+}
