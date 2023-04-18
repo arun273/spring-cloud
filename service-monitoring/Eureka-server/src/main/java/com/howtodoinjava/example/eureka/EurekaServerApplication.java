bd1c547803c06e2ce8272e030f7f84aa4c940949
arun273 <tupuriarun@gmail.com>
2023-04-18 10:15:11 +0530

    commit push
    commit push

---
+++ service-monitoring/Eureka-server/src/main/java/com/howtodoinjava/example/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.howtodoinjava.example.eureka;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@EnableAdminServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
